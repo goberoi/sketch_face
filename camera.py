d227c60a8ed5a2ffee9e6f2e41f1d15fd2d4e0a6
Gaurav Oberoi <goberoi@allenai.org>
2018-01-03 16:59:53 -0800

    Process nth frame.

--- camera.py
+++ camera.py
@@ -1,21 +1,13 @@
 import face_recognition
 import cv2
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
 # Initialize some variables
 face_locations = []
-process_this_frame = True
+process_nth_frame = 2
+frame_count = 0
 
 while True:
     # Grab a single frame of video
@@ -28,11 +20,12 @@ while True:
     rgb_small_frame = small_frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
-    if process_this_frame:
+    if (frame_count == 0):
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
 
-    process_this_frame = not process_this_frame
+    frame_count = (frame_count + 1) % process_nth_frame
+    print(frame_count)
 
     # Display the results
     for (top, right, bottom, left) in face_locations:
