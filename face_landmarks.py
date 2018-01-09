1bacd951e5eed8cb2c84e6e4ed504286aa3e5977
Gaurav Oberoi <goberoi@allenai.org>
2018-01-08 23:16:48 -0800

    Moved various options to command line.

--- face_landmarks.py
+++ face_landmarks.py
@@ -4,13 +4,24 @@ import pprint
 import numpy as np
 import quickdraw
 import random
+import argparse
 
-# Settings
-process_nth_frame = 2
-scale_frame = 4
-blank_canvas = True
-logging = True
-cartoon = True
+
+# Settings via command line args or defaults
+parser = argparse.ArgumentParser()
+parser.add_argument("--video", 
+                    help="show the camera's video feed in the background",
+                    action="store_true")
+parser.add_argument("--sketch", 
+                    help="show facial features as hand drawn images from the quick-draw dataset",
+                    action="store_true")
+parser.add_argument("--verbose", "-v",
+                    help="log a bunch of stuff",
+                    action="store_true")
+settings = vars(parser.parse_args())
+
+settings['process_nth_frame'] = 2
+settings['scale_frame'] = 4
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
@@ -20,41 +31,41 @@ face_landmarks_list = []
 frame_count = 0
 pp = pprint.PrettyPrinter(indent=4)
 canvas = None
-eye = random.choice(quickdraw.images['eye'])
+sketch_images = None
 
 # Helper
 def log(msg):
-    if logging:
+    if settings['verbose']:
         pp.pprint(msg)
 
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
-    # Capture height and width of window
+    # Capture height and width of window[
     height, width = frame.shape[:2]
 
     # Pick the background to draw on
-    if blank_canvas:
+    if settings['video']:
+        canvas = frame.copy()
+    else:
         canvas = np.zeros((height,width,3), np.uint8)
         canvas[:, :, :] = (255, 255, 255)
-    else:
-        canvas = frame.copy()
 
     # Resize frame of video to for faster face recognition processing
-    frame = cv2.resize(frame, (0, 0), fx=(1/scale_frame), fy=(1/scale_frame))
+    frame = cv2.resize(frame, (0, 0), fx=(1/settings['scale_frame']), fy=(1/settings['scale_frame']))
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_frame = frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
-    if (frame_count % process_nth_frame) == 0:
+    if (frame_count % settings['process_nth_frame']) == 0:
         # Find all the faces and face encodings in the current frame of video
         face_landmarks = face_recognition.face_landmarks(rgb_frame)
 
     # Generate random features every so often
-    if (frame_count % 10) == 0:
-        eye = random.choice(quickdraw.images['eye'])
+    if ((frame_count % 10) == 0) or (not sketch_images):
+        sketch_images = { name : random.choice(quickdraw.images[name]) for name in ['eye', 'mouth', 'nose']}
 
     # Increment counter to track nth frame to process
     frame_count = (frame_count + 1) % 10000000
@@ -66,7 +77,7 @@ while True:
         # Draw landmarks
         for landmark, points in face.items():
             np_points = np.array(points, dtype='int32')
-            np_points *= scale_frame
+            np_points *= settings['scale_frame']
 
             color = (156,156,156)
             close_polygon = False
@@ -75,8 +86,8 @@ while True:
                 close_polygon = True
                 centroid = np.mean(np_points, axis=0).astype('int')
                 cv2.circle(canvas, tuple(centroid), 5, color, 7)
-                if cartoon:
-                    quickdraw.render(canvas, centroid[0], centroid[1], eye, 0.2)
+                if settings['sketch']:
+                    quickdraw.render(canvas, centroid[0], centroid[1], sketch_images['eye'], 0.2)
                 else:
                     cv2.polylines(canvas, [np_points], close_polygon, color, 3)
             else:
