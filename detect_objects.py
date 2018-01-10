1b5fa2154ee9f11dd835fdd7317f66626b79c31f
Gaurav Oberoi <goberoi@allenai.org>
2018-01-10 09:56:50 -0800

    Basic detection pipeline working. No rendering yet and code needs cleanup.

--- detect_objects.py
+++ detect_objects.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import os
+import cv2
+import numpy as np
 
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
@@ -34,10 +36,7 @@ category_index = label_map_util.create_category_index(categories)
 
 # Definite input and output Tensors for detection_graph
 image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-# Each box represents a part of the image where a particular object was detected.
 detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-# Each score represent how level of confidence for each of the objects.
-# Score is shown on the result image, together with the class label.
 detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
 detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
@@ -45,4 +44,42 @@ num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 # Start session
 sess = tf.Session(graph=detection_graph)
 
+def detect_objects(image):
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_expanded = np.expand_dims(image, axis=0)
+    # Actual detection.
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: image_expanded})
+    return (boxes, scores, classes, num)
+
 print("got here")
+
+
+if __name__ == "__main__":
+    # Get a reference to webcam #0 (the default one)
+    video_capture = cv2.VideoCapture(0)
+
+    process_frame = True
+
+    while(True):
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+
+        if process_frame:
+            print("detect objects")
+            print(type(frame))
+            (boxes, scores, classes, num) = detect_objects(frame)
+            print(boxes)
+            print(scores)
+            print(classes)
+            print(num)
+
+        process_frame = (not process_frame)
+
+        cv2.imshow("video", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("breaking")
+            break
+
