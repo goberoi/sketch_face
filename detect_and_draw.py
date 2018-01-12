bd78b64d29f129b60e736a2980c47199deff3536
Gaurav Oberoi <goberoi@allenai.org>
2018-01-11 22:06:27 -0800

    Renamed some files.

--- detect_and_draw.py
+++ detect_and_draw.py
@@ -1,55 +1,122 @@
 import face_recognition
 import cv2
+import pprint
+import numpy as np
+import quickdraw
+import random
+import argparse
+from utils import FPS, WebcamVideoStream
+import detect_objects
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
+# Settings via command line args or defaults
+parser = argparse.ArgumentParser()
+parser.add_argument("--video", 
+                    help="show the camera's video feed in the background",
+                    action="store_true")
+parser.add_argument("--sketch", 
+                    help="show facial features as hand drawn images from the quick-draw dataset",
+                    action="store_true")
+settings = vars(parser.parse_args())
+
+settings['process_nth_frame'] = 1
+settings['scale_frame'] = 4
 
 # Initialize some variables
-face_locations = []
-process_nth_frame = 2
+face_landmarks_list = []
 frame_count = 0
+pp = pprint.PrettyPrinter(indent=4)
+canvas = None
+sketch_images = None
+width = 1280
+height = 720
+
+# Get a reference to webcam #0 (the default one)
+video_capture = WebcamVideoStream(src = 0, width = width, height = height).start()
+
+# Load object detection
+detect_objects.init()
+
+# Track fps
+fps = FPS().start()
 
 while True:
     # Grab a single frame of video
-    ret, frame = video_capture.read()
+    frame = video_capture.read()
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    # Pick the background to draw on
+    if settings['video']:
+        canvas = frame.copy()
+    else:
+        canvas = np.zeros((height,width,3), np.uint8)
+        canvas[:, :, :] = (255, 255, 255)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     # Only process every other frame of video to save time
-    if (frame_count == 0):
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
+    if (frame_count % settings['process_nth_frame']) == 0:
+        # Detect objects
+        detections = detect_objects.detect(frame)
+        # Resize frame of video to for faster face recognition processing
+        frame = cv2.resize(frame, (0, 0), fx=(1/settings['scale_frame']), fy=(1/settings['scale_frame']))
+        # Detect facial landmarks
+        face_landmarks = face_recognition.face_landmarks(frame)
 
-    frame_count = (frame_count + 1) % process_nth_frame
+    # Render boxes
+    canvas = detect_objects.render(canvas, detections, skip_classes = ['person'])
+
+    # Pick random sketches every so often
+    if ((frame_count % 10) == 0) or (not sketch_images):
+        sketch_images = { name : random.choice(quickdraw.images[name]) for name in ['eye', 'mouth', 'nose']}
+        sketch_images['nose_bridge'] = sketch_images['nose']
+        sketch_images['left_eye'] = sketch_images['eye']
+        sketch_images['right_eye'] = sketch_images['eye']
+
+    # Increment counter to track nth frame to process
+    frame_count = (frame_count + 1) % 10000000
 
     # Display the results
-    for (top, right, bottom, left) in face_locations:
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+    for face in face_landmarks:
+        # Draw landmarks
+        for landmark, points in face.items():
+            np_points = np.array(points, dtype='int32')
+            np_points *= settings['scale_frame']
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            color = (156,156,156)
+            close_polygon = False
 
-        # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, "face", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            if landmark in ['left_eye', 'right_eye', 'nose_bridge']:
+                close_polygon = True
+                centroid = np.mean(np_points, axis=0).astype('int')
+                sketch_image_scale = 0.2
+                if landmark in ['nose_bridge']:
+                    sketch_image_scale = 0.5
+#                cv2.circle(canvas, tuple(centroid), 5, color, 7)
+                if settings['sketch']:
+                    quickdraw.render(canvas, centroid[0], centroid[1], sketch_images[landmark], 0.2)
+                else:
+                    cv2.polylines(canvas, [np_points], close_polygon, color, 3)
+            elif landmark in ['nose_tip']:
+                pass
+            else:
+                cv2.polylines(canvas, [np_points], close_polygon, color, 3)
 
     # Display the resulting image
-    cv2.imshow('Video', frame)
+    cv2.imshow('Video', canvas)
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+    # Track FPS
+    fps.update()
+
+# Print time performance
+fps.stop()
+print('[INFO] elapsed time (total): {:.2f}'.format(fps.elapsed()))
+print('[INFO] approx. FPS: {:.2f}'.format(fps.fps()))
+
 # Release handle to the webcam
-video_capture.release()
+video_capture.stop()
 cv2.destroyAllWindows()
 
